425a181938c08412dacec3176317262305a7ced5
Johannes Langbein <jllangbein@gmail.com>
2025-11-28 12:43:05 +0100

    Inital Flutter Project Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# conversaition
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/conversaition/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.conversaition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
