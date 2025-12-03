775b3e74d5aa12f070a3965cf6c2769d04989d24
Johannes Langbein <jllangbein@gmail.com>
2025-12-03 15:04:23 +0100

    Addedf ChatGPT's supported languages and minor fixes to Dialog

--- lib/features/common/widgets/language_topic_selector_dialogue.dart
+++ lib/features/common/widgets/language_topic_selector_dialogue.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_final_fields
 
 import 'package:conversaition/features/chat/screens/chat_screen.dart';
+import 'package:conversaition/features/common/data/supported_languages.dart';
 import 'package:flutter/material.dart';
 import 'package:language_picker/language_picker_dropdown.dart';
 import 'package:language_picker/language_picker_dropdown_controller.dart';
@@ -18,7 +19,7 @@ class _LanguageTopicSelectorDialogueState
     extends State<LanguageTopicSelectorDialogue> {
   // Language Picker Controller
   LanguagePickerDropdownController _languageController =
-      LanguagePickerDropdownController(Languages.faroese);
+      LanguagePickerDropdownController(Languages.english);
   // Topic Input Controller
   final _topicController = TextEditingController();
 
@@ -61,85 +62,88 @@ class _LanguageTopicSelectorDialogueState
         children: [
           Text('Please select the language and the topic of the conversation!'),
           SizedBox(
-            width: 287.0,
+            width: 270.0,
             child: LanguagePickerDropdown(
               // the controller functionality doesn't seem to work properly
               // hence the manual asigning of the value in setState
               controller: _languageController,
+              languages: supportedLanguages,
               onValuePicked: (l) => setState(() {
-                debugPrint(l.name);
                 _languageController.value = l;
                 languagePicked = true;
               }),
             ),
           ),
-          DropdownMenu(
-            controller: _topicController,
-            enableSearch: false,
-            hintText: "Topic",
-            inputDecorationTheme: InputDecorationThemeData(
-              border: InputBorder.none,
-              suffixIconColor: Theme.of(context).colorScheme.outline,
-            ),
-            textStyle: Theme.of(context).textTheme.titleMedium,
-            width: double.infinity,
-            menuStyle: MenuStyle(
-              maximumSize: WidgetStatePropertyAll(Size.fromWidth(320.0)),
-            ),
+          SizedBox(
+            width: 286.0,
+            child: DropdownMenu(
+              controller: _topicController,
+              enableSearch: false,
+              hintText: "Topic",
+              inputDecorationTheme: InputDecorationThemeData(
+                border: InputBorder.none,
+                suffixIconColor: Theme.of(context).colorScheme.outline,
+              ),
+              textStyle: Theme.of(context).textTheme.titleMedium,
+              width: double.infinity,
+              menuStyle: MenuStyle(
+                maximumSize: WidgetStatePropertyAll(Size.fromWidth(320.0)),
+              ),
 
-            onSelected: (_) => setState(() {
-              topicPicked = true;
-            }),
-            dropdownMenuEntries: [
-              DropdownMenuEntry<String>(
-                value: 'Business',
-                label: 'Business',
-                labelWidget: Text(
-                  'Business',
-                  style: Theme.of(context).textTheme.titleMedium,
+              onSelected: (_) => setState(() {
+                topicPicked = true;
+              }),
+              dropdownMenuEntries: [
+                DropdownMenuEntry<String>(
+                  value: 'Business',
+                  label: 'Business',
+                  labelWidget: Text(
+                    'Business',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                 ),
-              ),
-              DropdownMenuEntry<String>(
-                value: 'Hobbies',
-                label: 'Hobbies',
-                labelWidget: Text(
-                  'Hobbies',
-                  style: Theme.of(context).textTheme.titleMedium,
+                DropdownMenuEntry<String>(
+                  value: 'Hobbies',
+                  label: 'Hobbies',
+                  labelWidget: Text(
+                    'Hobbies',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                 ),
-              ),
-              DropdownMenuEntry<String>(
-                value: 'Travel',
-                label: 'Travel',
-                labelWidget: Text(
-                  'Travel',
-                  style: Theme.of(context).textTheme.titleMedium,
+                DropdownMenuEntry<String>(
+                  value: 'Travel',
+                  label: 'Travel',
+                  labelWidget: Text(
+                    'Travel',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                 ),
-              ),
-              DropdownMenuEntry<String>(
-                value: 'Food',
-                label: 'Food',
-                labelWidget: Text(
-                  'Food',
-                  style: Theme.of(context).textTheme.titleMedium,
+                DropdownMenuEntry<String>(
+                  value: 'Food',
+                  label: 'Food',
+                  labelWidget: Text(
+                    'Food',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                 ),
-              ),
-              DropdownMenuEntry<String>(
-                value: 'Music',
-                label: 'Music',
-                labelWidget: Text(
-                  'Music',
-                  style: Theme.of(context).textTheme.titleMedium,
+                DropdownMenuEntry<String>(
+                  value: 'Music',
+                  label: 'Music',
+                  labelWidget: Text(
+                    'Music',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                 ),
-              ),
-              DropdownMenuEntry<String>(
-                value: 'Movies',
-                label: 'Movies',
-                labelWidget: Text(
-                  'Movies',
-                  style: Theme.of(context).textTheme.titleMedium,
+                DropdownMenuEntry<String>(
+                  value: 'Movies',
+                  label: 'Movies',
+                  labelWidget: Text(
+                    'Movies',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
@@ -151,7 +155,7 @@ class _LanguageTopicSelectorDialogueState
         ),
         FilledButton.icon(
           icon: Icon(Icons.check),
-          onPressed: _start,
+          onPressed: (languagePicked && topicPicked) ? _start : null,
           label: Text('Start'),
         ),
       ],
